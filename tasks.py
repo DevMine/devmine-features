9a7dbd99736bbd49317dcadc7f9213f87d9ef9bb
Daniel 'Akathorn' Espino <akathorn@gmail.com>
2014-05-05 14:31:19 +0200

    add tasks.py to invoke tasks

---
+++ tasks.py
@@ -0,0 +1,45 @@
+from invoke import (
+    task,
+    run
+)
+
+# define projects directories
+dirs = 'parsing'
+
+
+@task
+def pep8():
+    cmd = 'pep8 tasks.py ' + dirs
+    run_cmd(cmd)
+
+
+@task
+def pyflakes():
+    cmd = 'pyflakes tasks.py ' + dirs
+    run_cmd(cmd)
+
+
+@task('pep8', 'pyflakes')
+def check():
+    pass
+
+
+@task
+def clean():
+    run_cmd("find . -name '__pycache__' -exec rm -rf {} +")
+    run_cmd("find . -name '*.pyc' -exec rm -f {} +")
+    run_cmd("find . -name '*.pyo' -exec rm -f {} +")
+    run_cmd("find . -name '*~' -exec rm -f {} +")
+    run_cmd("find . -name '._*' -exec rm -f {} +")
+
+
+@task('clean')
+def clean_env():
+    run_cmd('rm -r ./env && mkdir env && touch env/.keep')
+
+
+def run_cmd(cmd):
+    "Run a system command verbosely."
+    print('Running \'' + cmd + '\'...')
+    run(cmd)
+    print('Done')
