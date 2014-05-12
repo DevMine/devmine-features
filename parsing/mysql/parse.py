05b8bec7213bdb14b4df1addf2eb400c701218e7
Daniel 'Akathorn' Espino <akathorn@gmail.com>
2014-05-12 22:48:12 +0200

    make the code pep8 compliant

--- parsing/mysql/parse.py
+++ parsing/mysql/parse.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2.7
-from __future__ import print_function as fprint
-from regexp import *
+from __future__ import print_function
+from mysql_regexp import *
 from table_fields import *
 import re
 
@@ -11,16 +11,18 @@ def parse_mysql(path):
     for table in table_regexp.keys():
         table_file[table] = open("dataset/tables/" + table, "w")
 
+    i = 0
     for l in open(path):
+        i += 1
         try:
             if l.startswith("INSERT INTO"):
                 table = l.split()[2][1:-1]
                 if table in table_regexp:
                     for match in re.finditer(table_regexp[table], l):
                         fields = [match.group(f) for f in table_fields[table]]
-                        fprint(",".join(fields), file=table_file[table])
+                        print(",".join(fields), file=table_file[table])
         except Exception as e:
-            fprint("error at line ", i, e)
+            print("error at line ", i, e)
 
 
 if __name__ == "__main__":
