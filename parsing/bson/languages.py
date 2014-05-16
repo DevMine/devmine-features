71d66c0f1f3fc0923ea5444b2d0e0edfcf5a2100
Daniel 'Akathorn' Espino <akathorn@gmail.com>
2014-05-16 21:28:59 +0200

    Fixed language feature computation

--- parsing/bson/languages.py
+++ parsing/bson/languages.py
@@ -1,31 +1,45 @@
+from __future__ import print_function
 import sys
+import os
+import tarfile
+from bsonstream import KeyValueBSONInput
 
 
-def read_languages(source, outdir):
+def read_languages(sourcedir, outdir):
     # Read
     user_langs = {}
     languages = set()
 
-    for line in source:
-        try:
-            user, language, lines = line.strip().split(",")
-            if(language != 'None'):
-                languages.add(language)
-                if user in user_langs:
-                    if language in user_langs[user]:
-                        user_langs[user][language] += int(lines)
+    for fname in os.listdir(sourcedir):
+        print("Opening " + fname)
+        f = tarfile.open(sourcedir + "/" + fname).extractfile("dump/github/repos.bson")
+        print("Parsing " + fname)
+        stream = KeyValueBSONInput(fh = f)
+
+        for _, repo in stream:
+            try:
+                user = repo["owner"]["login"]
+                language = repo["language"]
+                size = repo["size"]
+                if language:
+                    languages.add(language)
+                    if user in user_langs:
+                        if language not in user_langs[user]:
+                            user_langs[user][language] = 0
+                        user_langs[user][language] += int(size)
                     else:
-                        user_langs[user][language] = int(lines)
-                else:
-                    user_langs[user] = {language: int(lines)}
-        except Exception as e:
-            print("Problem reading line:", line.strip(), file=sys.stderr)
-            print(e, file=sys.stderr)
+                        user_langs[user] = {language: int(size)}
+            except Exception as e:
+                print("Problem reading line:", line.strip(), file=sys.stderr)
+                print(e, file=sys.stderr)
+
+        f.close()
 
     # Write
+    print("Writing to files")
     files = {}
     for language in languages:
-        files[language] = open(outdir + "/" + language + ".txt", "w")
+        files[language] = open(outdir + "/" + language.replace("/", "|"), "w")
 
     for user in user_langs:
         for l in user_langs[user]:
@@ -37,10 +51,7 @@ def read_languages(source, outdir):
         f.close()
 
 if __name__ == "__main__":
-    if(len(sys.argv) < 2):
-        print("Please specify an output directory")
-    elif(len(sys.argv) == 2):
-        read_languages(sys.stdin, sys.argv[1])
+    if(len(sys.argv) < 3):
+        print("Usage: python languages.py inputdir outputdir")
     else:
-        print(sys.argv[1], sys.argv[2])
-        read_languages(open(sys.argv[1]), sys.argv[2])
+        read_languages(sys.argv[1], sys.argv[2])
